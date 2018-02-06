fb277ef1f25aa5084bcb11a1b3b804a500807fb7
Ansar Samad <Ansar.Samad@harlandfs.com>
2018-02-06 14:58:13 +0530

    Signed-off-by: Ansar Samad <Ansar.Samad@harlandfs.com>

--- watsonConversation.js
+++ watsonConversation.js
@@ -1,4 +1,6 @@
 var watson = require('watson-developer-cloud');
+const NodeCache = require( "node-cache" );
+const myCache = new NodeCache();
 
 var conversation = watson.conversation({
 username: 'ce4418be-0dc5-4301-8374-5a37c680c68b',
@@ -10,14 +12,28 @@ version_date: '2017-05-26'
 var getResponse = (userInput)=>{
 
     return new Promise((resolve,reject)=>{
+        //try sychronous mode
+        previous_context = myCache.get( "previous-context" );
+        if(previous_context ==  undefined){
+            previous_context={}
+        }
+        console.log('previous context:'+JSON.stringify(previous_context));
         conversation.message({
-            workspace_id: '2164b471-3806-4da2-8411-dc2ff0b24832',
-            input: {'text': userInput}
-            },  function(err, response) {
+            workspace_id : '2164b471-3806-4da2-8411-dc2ff0b24832',
+            input : {'text': userInput},
+            context : previous_context
+            },  (err, response)=> {
                 if (err){
                     console.log('error:', err);
                     reject('Error Occured..'+err);
-                } else{
+                } else{                    
+                    myCache.set("previous-context",response.context,(err,success)=>{
+                        if(err){
+                            console.log('error occured while caching response');
+                        }else{
+                            console.log('watson context stored in cache successfully');
+                        }
+                    })
                     console.log(JSON.stringify(response, null, 2));
                     resolve(response.output.text[0]);
                 }
